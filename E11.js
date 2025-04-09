ce50534d5bce81f26d0e93b5af33cb5d8b49d89c
sbondia <sbondia>
2025-04-09 11:09:42 +0200

    E113

--- E11.js
+++ E11.js
@@ -5,3 +5,12 @@ htmlOutput("E111", add)
 
 const randomNumber = () => Math.floor(Math.random()*100)
 htmlOutput("E112", randomNumber)
+
+class Person{
+    constructor(){
+    this.name = "name"
+    }
+    greet = () => console.log(`Hola, ${this.name}.`)
+}
+const person = new Person()
+htmlOutput("E113", person.greet)
--- locallib.js
+++ locallib.js
@@ -1,5 +1,6 @@
 "use strict"
 function htmlOutput(id, arrow){
     const aux = document.getElementById(`${id}`)
-    aux.innerHTML = arrow
+    aux.innerHTML += arrow+'<br>Output: '
+    aux.innerHTML += arrow() || "Este sale por consola o no tiene salida"
 }
