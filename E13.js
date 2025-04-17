94b00c834b32b6869d951432ba8bdb42e576d57f
sbondia <sbondia>
2025-04-17 18:49:28 +0200

    E133

--- E13.js
+++ E13.js
@@ -21,3 +21,13 @@ function calculadora(num1, num2, callback){
     return callback(num1, num2)
 }
 outputConsole("E132", calculadora, num3, num4, suma)
+
+//E133
+let nom = "David"
+function funcNom(nom){
+    return nom
+}
+function esperarISaludar(nom, func){
+    setTimeout(()=> func(nom), 2000)
+}
+outputConsole("E133", esperarISaludar, nom, funcNom)
