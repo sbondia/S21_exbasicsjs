7594528df80d1fa4a9171cacb9f2ea2605bad243
sbondia <sbondia>
2025-04-20 19:59:45 +0200

    E161+E162

---
+++ E16.js
@@ -0,0 +1,11 @@
+"use strict"
+outputConsoleTitle("E16")
+
+//E161
+const noms161 = ['Anna', 'Bernat', 'Clara']
+//noms161.forEach(nom=> console.log(nom))
+outputConsole("E161", noms161)
+
+//E162
+//for(let nom of noms161) {console.log(nom)}
+outputConsole("E162", noms161)
