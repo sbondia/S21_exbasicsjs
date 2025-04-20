b20e2ac20d916bad879f1d3306e3140437514401
sbondia <sbondia>
2025-04-20 19:24:38 +0200

    E152

--- E15.js
+++ E15.js
@@ -3,6 +3,12 @@ outputConsoleTitle("E15")
 
 //E151
 const array151 = [1, 2, 3, 4]
-const array151_2 = array151.map(val=> val*val)
+const array151_2 = array151.map(num=> num*num)
 outputConsole("E151", array151_2)
 //console.log(array151_2)
+
+//E152
+const array152 = [1, 2, 3, 4]
+const array152_2 = array152.filter(num=> num%2==0)
+outputConsole("E152", array152_2)
+//console.log(array152_2)
