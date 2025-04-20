98f8ff221d074f701acfabb1c71415d20fc4feac
sbondia <sbondia>
2025-04-20 19:14:25 +0200

    E151

---
+++ E15.js
@@ -0,0 +1,8 @@
+"use strict"
+outputConsoleTitle("E15")
+
+//E151
+const array151 = [1, 2, 3, 4]
+const array151_2 = array151.map(val=> val*val)
+outputConsole("E151", array151_2)
+//console.log(array151_2)
