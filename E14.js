9248d3454cd75ab240e73af1e9b78cb3ac6a00cb
sbondia <sbondia>
2025-04-20 13:56:01 +0200

    E146

--- E14.js
+++ E14.js
@@ -19,14 +19,14 @@ outputConsole("E142", suma142, num1, num2, num3, num4)
 const obj1 = {atr1: 1, atr2: 2}
 const obj2 = {...obj1}
 obj2.atr1 = 11
-//console.log(obj2)
 outputConsole("E143", obj2)
+//console.log(obj2)
 
 //E144
 const array44 = ["azul", "rojo", 1, 2, 3]
 let [color1, color2, ...numeros] = array44
-//console.log(color1, color2, numeros)
 outputConsole("E144", array44)
+//console.log(color1, color2, numeros)
 
 //E145
 const array45 = [1, 2, 3]
@@ -34,3 +34,10 @@ function spreadFunc(arg1, arg2, arg3){
     return arg1+arg2+arg3
 }
 outputConsole("E145", spreadFunc, ...array45)
+
+//E146
+const obj3 = {atr1: 1, atr2: 2}
+const obj4 = {atr3: 3, atr4: 4}
+const obj34 = {...obj3, ...obj4}
+outputConsole("E146", obj34)
+//console.log(obj34)
