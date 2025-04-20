f0aca93469b879a2bfc8af77e6db9473ad72802d
sbondia <sbondia>
2025-04-20 20:03:33 +0200

    E163

--- E16.js
+++ E16.js
@@ -9,3 +9,8 @@ outputConsole("E161", noms161)
 //E162
 //for(let nom of noms161) {console.log(nom)}
 outputConsole("E162", noms161)
+
+//E163
+const array163 = [1, 2, 3, 4, 5, 6]
+const array163_2 = array163.filter(num=> num%2==0)
+outputConsole("E163", array163_2)
