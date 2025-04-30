24ee83f5279c923180b93c6c0eeb690c8b82d06a
Sergi Bondia <sbondia716@gmail.com>
2025-04-30 10:34:33 +0200

    E173

--- E17.js
+++ E17.js
@@ -3,7 +3,6 @@ outputConsoleTitle("E17")
 
 //let myPromise = new Promise(funcion-codigolargo);
 //myPromise.then(funcion-codigoenespera);
-//setTimeout(()=> {resolve(console.log("Hola, món")), 2000}
 
 //E171
 const myPromise = new Promise((resolve)=> {
@@ -12,3 +11,22 @@ const myPromise = new Promise((resolve)=> {
 
 //E172
 myPromise.then((salida)=> console.log(salida))
+
+//E173
+/*
+const myPromise2 = new Promise((resolve,reject)=>{
+    let x = false
+    setTimeout(()=>{
+        if(x){
+            return  x1(console.log("Hola, món", x1, Promise, myPromise))
+        }else{
+            return x2(console.log("Hola, men", x2, Promise, myPromise))
+        }
+    },2000)
+})
+*/
+let input = 'hoola'
+const myPromise173 = new Promise((resolve, reject)=> {
+    input == 'hola' ? setTimeout(()=>resolve("Hola, món"), 2000) : reject
+})
+myPromise173.then((salida)=> console.log(salida))
